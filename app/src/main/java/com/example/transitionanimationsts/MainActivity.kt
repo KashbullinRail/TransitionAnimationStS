cc6197a5963e5cd217ac4603f667ab3cbfeb2245
KashbullinRail <kashbullinrail@gmail.com>
2022-12-08 20:20:27 +0300

    add DoubleBackPressed

--- app/src/main/java/com/example/transitionanimationsts/MainActivity.kt
+++ app/src/main/java/com/example/transitionanimationsts/MainActivity.kt
@@ -2,22 +2,35 @@ package com.example.transitionanimationsts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
+    private var backPressed = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupActionBarWithNavController(findNavController(R.id.fragmentCont))
+        setupActionBarWithNavController(findNavController(R.id.fragmentContainer))
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.fragmentCont)
+        val navController = findNavController(R.id.fragmentContainer)
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        if (backPressed + 2000 > System.currentTimeMillis()){
+            super.onBackPressed()
+        } else {
+            Toast.makeText(this, "Press back pressed", Toast.LENGTH_SHORT)
+        }
+        backPressed = System.currentTimeMillis()
+
     }
 
 }
--- app/src/main/java/com/example/transitionanimationsts/fragments/ThirdFragment.kt
+++ app/src/main/java/com/example/transitionanimationsts/fragments/ThirdFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.navigation.fragment.findNavController
 import com.example.transitionanimationsts.R
 import kotlinx.android.synthetic.main.fragment_third.view.*
